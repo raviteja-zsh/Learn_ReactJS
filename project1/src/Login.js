49ae6d4aaa0e3bcec741bd9dad4c9e1a746b657a
raviteja-zsh <tejaravi883@gmail.com>
2024-10-21 22:37:56 +0530

    added login functionality

--- project1/src/Login.js
+++ project1/src/Login.js
@@ -1,7 +1,24 @@
 
 function Login(){
     return( //should use () instead of {}
-        <h1>hello from world part 2 </h1>
+        <div className="container">
+           <div className="row">
+            <div className="col-4">
+            <h3>Login</h3>
+                <div className="mb-4">
+                <label>Email</label>
+                <input type="text" className="form-control" placeholder="Email"></input>
+                </div>
+                <div className="mb-4">
+                <label>Password</label>
+                <input type="password" className="form-control" placeholder="password"></input>
+                </div> 
+                <div>
+                    <button className="btn btn-primary">Login</button>
+                    </div>               
+            </div>
+           </div>
+        </div>
     )
 }
 
